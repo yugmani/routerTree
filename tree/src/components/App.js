2012b0d835bcd40be9f48e6e9b774b93df00a152
Himalaya <yugmani@yahoo.com>
2021-01-27 16:05:32 -0800

    Route with URL parameter

--- tree/src/components/App.js
+++ tree/src/components/App.js
@@ -6,6 +6,7 @@ import About from "./About";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
 import NotFound from "./NotFound";
+import Featured from "./Featured";
 
 const App = () => (
   <BrowserRouter>
@@ -14,7 +15,8 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" render={() => <About title="ABOUT" />} />
-        <Route path="/teachers" component={Teachers} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/teachers/:topic/:name" component={Featured} />
         <Route path="/courses" component={Courses} />
         <Route component={NotFound} />
       </Switch>
